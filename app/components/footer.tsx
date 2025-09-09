514e106fbc68bd55d92e90e3021e31c9c75d452c
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-09 23:20:57 +0530

    Ui:navbar & footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,24 +2,24 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 export default function Footer() {
 
-    return <div className="w-full border-t border-gray-700 ">
+    return <div className="w-full border-t border-gray-500 ">
         <footer >
-            <div className="flex justify-between px-6 pt-22">
-                 <div className="flex flex-col space-y-4">
+            <div className="flex flex-col md:flex-row justify-between px-6 pt-12 md:pt-22">
+                 <div className="flex flex-col items-center md:items-start space-y-4">
                 <img src="/logo.svg" height={150} width={150} />
-                <div className="text-lg text-gray-600">
+                <div className="text-lg text-gray-600 text-wrap text-center md:text-start">
                     <h1>Automate Your B2B LinkedIn Outreach</h1>
                     <h1>Made with ❤️ by Sales Leaders</h1>
                 </div>
                 <div>
-                     <Button className="bg-blue-700 hover:bg-blue-600 cursor-pointer mt-4">
+                     <Button className=" bg-blue-700 hover:bg-blue-600 cursor-pointer mt-4">
                     Get Started Now
                 </Button>
                 </div>
                
 
             </div>
-            <div className="grid grid-cols-3 gap-12">
+            <div className="grid grid-cols-2 mt-8 md:mt-1 md:grid-cols-3 gap-12">
                 <div className="flex flex-col space-y-4">
                     <h1 className="text-xl font-semibold text-gray-700">Product</h1>
                     <Link className="linktext" href="/auth/login">Login</Link>
@@ -46,6 +46,8 @@ export default function Footer() {
 
             </div>
             </div>
+
+
             <div className="  border-t border-gray-300 shadow-md mt-8">
                 <p className="text-center text-gray-500 text-sm pt-4">LinkBird-LinkedIn automation tool for agencies, sales teams and GTM operators &#169; 2025</p>
             </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,22 +1,44 @@
+"use client"
 import { Button } from "@/components/ui/button";
-
-
+import { useState } from "react";
+import { X, Menu } from "lucide-react"
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
 
-    return <div className="w-full flex justify-center sticky top-0 ">
-        <nav className="  px-8 rounded-2xl shadow-md bg-white mt-6 py-3 flex items-center justify-between w-7xl">
-            <img src="/logo.svg" className="" alt="" height={150} width={150} />
+    return <div className="w-full flex justify-center sticky top-0 z-50 ">
+        <nav className=" px-6 md:px-8 rounded-2xl shadow-md bg-white mt-6 py-3 flex items-center justify-between w-full max-w-6xl">
+            <img src="/logo.svg" alt="Logo" height={150} width={150} />
 
-            <ul className="flex gap-12 text-lg text-gray-600 font-semibold">
-                <li>Home</li>
-                <li>Features</li>
-                <li>Pricing</li>
+            <ul className="hidden md:flex gap-12 text-lg text-gray-600 ">
+                <li className="hover:cursor-pointer hover:text-blue-700">Home</li>
+                <li className="hover:cursor-pointer hover:text-blue-700">Features</li>
+                <li className="hover:cursor-pointer hover:text-blue-700">Pricing</li>
             </ul>
-            <div>
+
+            <div className="hidden md:block">
                 <Button className="bg-blue-700 hover:bg-blue-600 cursor-pointer">
                     Get Started
                 </Button>
             </div>
+
+            <button className="md:hidden text-gray-800" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
+
+            {isOpen && (
+                <div className="absolute top-25 left-1 w-full bg-white shadow-md rounded-xl px-6 mr-4 py-4 md:hidden z-40">
+                    <ul className="flex flex-col gap-4 text-lg text-gray-600">
+                        <li className="hover:cursor-pointer hover:text-blue-700">Home</li>
+                        <li className="hover:cursor-pointer hover:text-blue-700">Feature</li>
+                        <li className="hover:cursor-pointer hover:text-blue-700">Pricing</li>
+                    </ul>
+                    <div className="mt-6 ">
+                        <Button className="w-full bg-blue-700 hover:bg-blue-600 cursor-pointer">
+                            Get Started
+                        </Button>
+                    </div>
+                </div>
+            )}
         </nav>
     </div>
 
