92963948d385b0387c1537bbcb742672473c0ccf
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-17 11:47:44 +0530

    feat:loadingdots component

---
+++ app/dashboard/components/LoadingDot.tsx
@@ -0,0 +1,12 @@
+
+"use client";
+
+export default function LoadingDots() {
+  return (
+    <div className="flex items-center justify-center space-x-2">
+      <span className="w-2 h-2 bg-blue-600 rounded-full animate-bounce [animation-delay:-0.3s]" />
+      <span className="w-2 h-2 bg-blue-600 rounded-full animate-bounce [animation-delay:-0.15s]" />
+      <span className="w-2 h-2 bg-blue-600 rounded-full animate-bounce" />
+    </div>
+  );
+}
