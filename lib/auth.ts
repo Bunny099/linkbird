761aaf46ac3f8a433a805b7febed17ad71075708
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-17 11:46:10 +0530

    fix:session time out

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,4 @@
-import { betterAuth, createMiddleware } from "better-auth"
-import { Content } from "next/font/google"
+import { betterAuth } from "better-auth"
 import { db } from "@/db/index";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { schema } from "@/db/schema";
@@ -7,19 +6,27 @@ import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
 
-    database: drizzleAdapter(db,{
+    database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema
     }),
-    emailAndPassword:{
-        enabled:true
+    emailAndPassword: {
+        enabled: true
     },
     socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID as string, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
     },
-  plugins:[nextCookies()]
-  
+    plugins: [nextCookies()],
+    secret: process.env.BETTER_AUTH_SECRET,
+    session: {
+
+        expiresIn: 60 * 60,
+
+    },
+
+
+
 })
