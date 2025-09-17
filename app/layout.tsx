6b9043c9fbf6fd41bd687a2f027ae7a8925a4f03
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-17 15:53:00 +0530

    layout:metadata and ico icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,40 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Linkfly",
-  description: "Glow your LinkedIn",
-};
+  description:
+    "Linkfly helps you craft a professional LinkedIn profile, attract recruiters, and grow your network with ease.",
+  keywords: ["Linkfly", "LinkedIn optimizer", "profile enhancer", "post generator"],
+  authors: [{ name: "Jayesh Khuman (Linkfly)" }],
+  openGraph: {
+    title: "Linkfly – Optimize Your LinkedIn Profile",
+    description:
+      "Boost your LinkedIn presence with AI-powered profile optimization and content generation.",
+    url: "https://linkfly-three.vercel.app",
+    siteName: "Linkfly",
+    images: [
+      {
+        url: "https://linkfly-three.vercel.app/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "Linkfly Dashboard",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Linkfly – Optimize Your LinkedIn Profile",
+    description:
+      "AI-powered LinkedIn profile optimization to stand out to recruiters.",
+    images: ["https://linkfly-three.vercel.app/og-image.png"],
+   
+  },
+  icons: {
+    icon: "/fly.ico", 
+  },
+  metadataBase: new URL("https://linkfly-three.vercel.app"),
+}
 
 export default function RootLayout({
   children,
