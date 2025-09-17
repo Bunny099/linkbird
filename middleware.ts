1e93977b93b5edc30e2f84333a385824c1e45022
Bunny099 <jayeshkhuman121@gmail.com>
2025-09-17 11:40:56 +0530

    fix: update the protected routes

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/dashboard", "/dashboard/campaign","/dashboard/leads"],
+	matcher: ["/dashboard", "/dashboard/post","/dashboard/optimizer"],
 };
